f94703667de3c097261905e13918390cf0db6412
Liromanz <kiss_wiwa_2003@mail.ru>
2023-09-11 15:06:22 +0300

    первый раз отправляю

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string txt;
+do
+{
+    Console.WriteLine("Какой цвет вы любите?");
+    Console.WriteLine("1. Черный");
+    Console.WriteLine("2. Белый");
+    Console.WriteLine("3. Розовый");
+    Console.WriteLine("4. Фиолетовый");
+    Console.WriteLine("5. никакой, отстань");
+    txt = Console.ReadLine();
+
+} while (txt != "5");
